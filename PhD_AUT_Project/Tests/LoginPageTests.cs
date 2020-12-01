10078b218b5826727ba9d79019a4f1016976d965
Sivanag-Git <sivanagit@gmail.com>
2020-12-01 19:24:16 +0530

    Revert "Update LoginPageTests.cs"
    This reverts commit 4bfea48d970be88916501c1e9e886bce3cbf35d2.

--- PhD_AUT_Project/Tests/LoginPageTests.cs
+++ PhD_AUT_Project/Tests/LoginPageTests.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace PhD_AUT_Project.Tests
 {
-    [Trait("Category", "Login Tests1")]
+    [Trait("Category", "Login Tests")]
     public class LoginPageTests
     {
         [Theory]
