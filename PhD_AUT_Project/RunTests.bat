f0732ee04b9af468a06c37f5a56ef3c81632a7f3
Sivanag-Git <sivanagit@gmail.com>
2020-12-01 16:08:18 +0530

    final

--- PhD_AUT_Project/RunTests.bat
+++
@@ -1,5 +0,0 @@
-cd C:\Users\Admin\source\repos\PhD_AUT_Project\PhD_AUT_Project
-
-dotnet test PhD_AUT_Project.csproj /p:CollectCoverage=true /p:IncludeTestAssembly=true /p:CoverletOutputFormat=cobertura
-
-reportgenerator "-reports:C:\Users\Admin\source\repos\PhD_AUT_Project\PhD_AUT_Project\coverage.cobertura.xml" "-targetdir:coveragereport" -reporttypes:Html
