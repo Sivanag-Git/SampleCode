b23dc31e5a7650aba0ffc8b5eac27e83bb3b48c1
Sivanag-Git <sivanagit@gmail.com>
2020-12-01 19:19:40 +0530

    Revert "1"
    This reverts commit dd30c393af60606aff5fb6398d92001bf6b550ac.

--- PhD_AUT_Project/Tests/LoginPageTests.cs
+++ PhD_AUT_Project/Tests/LoginPageTests.cs
@@ -38,10 +38,5 @@ namespace PhD_AUT_Project.Tests
             loginPage.confirmPassword = confirmPassword;
             Assert.Equal(pageTitle, loginPage.Submit().pageTitle);
         }
-
-        public void TestVS()
-        {
-            Assert.Equal(1, 1);
-        }
     }
 }
