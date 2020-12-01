525f65af77927224127a5b2e0f3abe83aee0f31e
Sivanag-Git <sivanagit@gmail.com>
2020-12-01 16:08:33 +0530

    commit final

---
+++ PhD_AUT_Project/Pages/LoginPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhD_AUT_Project
+{
+    public class LoginPage:Page
+    {
+        public LoginPage()
+        {
+            currentPage = PageIndex.LoginPage;
+            pageTitle = "Login";
+            loginUserName = "";
+        }
+
+        public string loginName { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+        public Page Submit()
+        {
+            if(loginName.Contains("User") && password.Contains("pwd"))
+            {
+                if (password == confirmPassword)
+                {
+                    return new MainPage(loginName);
+                }
+            }
+            return this;
+        }
+
+        public PageIndex Cancel()
+        {
+            loginName = "";
+            password = "";
+            return currentPage;
+        }
+    }
+}
---
+++ PhD_AUT_Project/Pages/Page.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhD_AUT_Project
+{
+    public enum PageIndex
+    {
+        LoginPage = 1,
+        MainPage = 2
+    }
+
+    public class Page
+    {
+        public PageIndex currentPage { get; set; }
+        public string pageTitle { get; set; }
+        public string loginUserName { get; set; }
+    }
+}
